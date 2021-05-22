4e437a038afb7dfb468935c1f41d70377ea6d27e
ryokoko <llzstella@yahoo.com>
2021-05-23 01:07:23 +1000

    Configured website 2

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    mode: 'development',
+    entry:'./index.js',
+    module: {
+        rules: [
+          {
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-react']
+              }
+            }
+          }, {
+              test: /\.css$/,
+              use:['style-loader', 'css-loader']
+          } 
+        ]
+      }
+}
