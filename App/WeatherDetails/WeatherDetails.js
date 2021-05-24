8b10d5ec7278f7e8a958982432298063780004a1
ryokoko <llzstella@yahoo.com>
2021-05-25 01:01:56 +1000

    set blur effect of right pane

--- App/WeatherDetails/WeatherDetails.js
+++ App/WeatherDetails/WeatherDetails.js
@@ -1,15 +1,20 @@
 import React from 'react';
 
+import Flex from '../../Components/Flex';
+
+import SearchBar from './App/SearchBar';
 import styled from 'styled-components';
 
-const WeatherDetailWrapper = styled.div`
+const WeatherDetailWrapper = styled(Flex)`
     flex: 35%;
     background-color: #5f6e6642;
+    backdrop-filter: blur(6px);
+    flex-direction: column;
 `;
 
 const WeatherDetails = () => (
     <WeatherDetailWrapper>
-        not yet started;
+        <SearchBar />
     </WeatherDetailWrapper>
 
 )
