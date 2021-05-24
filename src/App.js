d9c0839a8e2cc0d1d18767598a59b07f43c7d333
ryokoko <llzstella@yahoo.com>
2021-05-24 14:03:36 +1000

    commit from console

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+/* import Header from '../App/Header'; */
+
+import Background from '../App/Background';
+/* import WeatherContainer from '../App/WeatherContainer';
+import Footer from '../App/Footer';
+ */
+
+ /* <Header />
+                        <WeatherContainer />
+                        <Footer /> */
+
+const App = () => (
+        <div>
+                <Background />
+        </div>   
+)
+
+export default App;
